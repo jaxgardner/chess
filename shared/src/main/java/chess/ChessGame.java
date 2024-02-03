3407d0c076f59d98516071de6b1999e939a4cf5a
Jaxon Gardner <pjaxongardner@gmail.com>
2024-02-02 19:15:23 -0700

    Fixed changing moves logic

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -145,6 +145,7 @@ public class ChessGame {
         HashSet<ChessMove> validMovesList = (HashSet<ChessMove>) validMoves(move.getStartPosition());
         if(validMovesList.contains(move)) {
             movePiece(gameBoard, move);
+            currentTurn = currentTurn == TeamColor.WHITE ? TeamColor.BLACK : TeamColor.WHITE;
         }
         else {
             throw new InvalidMoveException("Move is not valid");
