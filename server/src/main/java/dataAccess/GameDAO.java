81657a8480c6053fae647d0b98c2e962c8670eb0
Jaxon Gardner <pjaxongardner@gmail.com>
2024-02-16 23:23:32 -0700

    Implemented gamedao class

--- server/src/main/java/dataAccess/GameDAO.java
+++ server/src/main/java/dataAccess/GameDAO.java
@@ -3,15 +3,19 @@ package dataAccess;
 import dataAccess.Exceptions.DataAccessException;
 import model.GameData;
 
+import java.util.ArrayList;
+
 public interface GameDAO {
-    public GameData createGame(String gameName, int gameID) throws DataAccessException;
+    public void addGame(String gameName, int gameID) throws DataAccessException;
 
-    public GameData getGame();
+    public GameData getGame(int gameID) throws DataAccessException;
 
-    public GameData listGames();
+    public ArrayList<GameData> listGames() throws DataAccessException;
 
-    public void updateGame();
+    public void updateGameWhite(String playerColor, String username, int gameID) throws DataAccessException;
 
-    public void clear();
+    public void updateGameBlack(String playerColor, String username, int gameID) throws DataAccessException;
+
+    public void clear() throws DataAccessException;
 
 }
--- server/src/main/java/dataAccess/Memory/MemGameDao.java
+++ server/src/main/java/dataAccess/Memory/MemGameDao.java
@@ -1,19 +1,86 @@
 package dataAccess.Memory;
 
+import chess.ChessGame;
+import dataAccess.Exceptions.DataAccessException;
 import dataAccess.GameDAO;
 import model.GameData;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class MemGameDao implements GameDAO {
-    HashMap<String, GameData> gameData;
+    HashMap<Integer, GameData> gameDataStorage;
 
     public MemGameDao() {
-        gameData = new HashMap<>();
+        gameDataStorage = new HashMap<>();
     }
 
-    public GameData addGame() {
+    public void addGame(String gameName, int gameID) throws DataAccessException {
+        GameData newGameData = new GameData(gameID, "", "", gameName, new ChessGame());
+        try {
+            gameDataStorage.put((Integer) gameID, newGameData);
+        } catch (Exception e) {
+            throw new DataAccessException("Cannot connect to server");
+        }
+    }
 
+    public GameData getGame(int gameID) throws DataAccessException {
+        GameData gameData;
+
+        try {
+            gameData = gameDataStorage.get((Integer) gameID);
+        } catch (Exception e) {
+            throw new DataAccessException("Cannot connect to server");
+        }
+        return gameData;
+    }
+
+    public ArrayList<GameData> listGames() throws DataAccessException {
+        ArrayList<GameData> games = new ArrayList<>();
+        try {
+            for(Map.Entry<Integer, GameData> entry: gameDataStorage.entrySet()) {
+                GameData game = entry.getValue();
+                games.add(game);
+            }
+        } catch (Exception e) {
+            throw new DataAccessException("Cannot connect to server");
+        }
+        return games;
+    }
+
+    public void updateGameWhite(String playerColor, String username, int gameID) throws DataAccessException {
+        try {
+            GameData game = getGame(gameID);
+            GameData updatedGame = null;
+            if(game != null) {
+                updatedGame = new GameData(gameID, username, game.blackUsername(), game.gameName(), game.game());
+            }
+            gameDataStorage.put((Integer) gameID, updatedGame);
+        } catch (Exception e) {
+            throw new DataAccessException("Cannot connect to server");
+        }
+    }
+
+    public void updateGameBlack(String playerColor, String username, int gameID) throws DataAccessException {
+        try {
+            GameData game = getGame(gameID);
+            GameData updatedGame = null;
+            if(game != null) {
+                updatedGame = new GameData(gameID, game.whiteUsername(), username, game.gameName(), game.game());
+            }
+            gameDataStorage.put((Integer) gameID, updatedGame);
+        } catch (Exception e) {
+            throw new DataAccessException("Cannot connect to server");
+        }
+    }
+
+    public void clear() throws DataAccessException {
+        try {
+            gameDataStorage.clear();
+        } catch (Exception e) {
+            throw new DataAccessException("Cannot connect to server");
+        }
     }
 
 }
