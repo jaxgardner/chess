d391ce96bf46505675222f3b2cb1453931a35ad1
Jaxon Gardner <pjaxongardner@gmail.com>
2024-02-16 18:26:46 -0700

    Implemented userdao class

--- server/src/main/java/dataAccess/UserDAO.java
+++ server/src/main/java/dataAccess/UserDAO.java
@@ -1,11 +1,15 @@
 package dataAccess;
 
+
 import model.UserData;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public interface UserDAO {
-    public UserData createUser();
+    public UserData createUser(String username, String password, String email);
 
-    public UserData getUser();
-
+    public UserData getUser(String username) throws DataAccessException;
 
 }
+
