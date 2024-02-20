4a78313c84e71870b4079f412bc148ebf2d35007
Jaxon Gardner <pjaxongardner@gmail.com>
2024-02-19 22:28:30 -0700

    Basic implementation of login service

--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,41 +1,68 @@
 package service;
 
+import dataAccess.Memory.MemAuthDao;
+import dataAccess.Memory.MemUserDao;
 import model.AuthData;
+import model.UserData;
 import models.LoginRequest;
+
+import java.util.Objects;
+import java.util.UUID;
 
 public class LoginService {
 
-    private boolean checkPassword(LoginRequest req) {
+    private final MemUserDao userDAO;
+    private final MemAuthDao authDAO;
 
+    public LoginService() {
+        userDAO = new MemUserDao();
+        authDAO = new MemAuthDao();
     }
 
-    private boolean checkAuth(AuthData req) {
-
+    private UserData getUser(String username) throws Exception {
+        return userDAO.getUser(username);
     }
 
+    private boolean checkLoginPassword(LoginRequest userLogin) throws Exception {
+        UserData userFromDB = getUser(userLogin.username());
 
-    private String generateAuth() {
-
+        return userFromDB != null && Objects.equals(userLogin.password(), userFromDB.password());
     }
 
-    private void addToAuthData(String auth) {
-
+    private boolean checkAuthToken(AuthData req) throws Exception {
+        return authDAO.getAuth(req.authToken());
     }
 
-    private void deleteFromAuthData(AuthData req) {}
+    private AuthData generateAuth(String username) throws Exception {
+        UUID uuid = UUID.randomUUID();
 
-    public AuthData getLogin(LoginRequest req) {
-        String auth = null;
-        if(checkPassword(req)) {
-            auth = generateAuth();
-            addToAuthData(auth);
+        String uuidString = uuid.toString();
+
+        AuthData newUserAuth = new AuthData(username, uuidString);
+
+        return authDAO.createAuth(newUserAuth);
+    }
+
+    private void addToAuthData(AuthData userAuth) throws Exception {
+        authDAO.createAuth(userAuth);
+    }
+
+    private void deleteFromAuthData(AuthData req) throws Exception {
+        authDAO.deleteAuth(req.authToken());
+    }
+
+    public AuthData getLogin(LoginRequest req) throws Exception {
+        AuthData newUserAuth = null;
+        if(checkLoginPassword(req)) {
+            newUserAuth = generateAuth(req.username());
+            addToAuthData(newUserAuth);
         }
 
-        return new AuthData(req.username(), auth);
+        return newUserAuth;
     }
 
-    public void getLogout(AuthData req) {
-        if(checkAuth(req)) {
+    public void getLogout(AuthData req) throws Exception {
+        if(checkAuthToken(req)) {
             deleteFromAuthData(req);
         }
     }
