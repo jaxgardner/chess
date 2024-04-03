2e4e11f9b0e0bbbbde4666e031aa2b812f9ed500
Jaxon Gardner <pjaxongardner@gmail.com>
2024-04-02 18:39:03 -0600

    Built websocket connection handlers

--- server/src/main/java/websocket/ConnectionManager.java
+++ server/src/main/java/websocket/ConnectionManager.java
@@ -1,4 +1,48 @@
 package websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+
+import javax.management.Notification;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ConnectionManager {
+    public final ConcurrentHashMap<Integer, ArrayList<Connection>> gameConnections = new ConcurrentHashMap<>();
+
+    public void add(Integer gameID, String username, Session session) {
+        var connection = new Connection(username, session);
+        if(gameConnections.containsKey(gameID)) {
+            var gameObservers = gameConnections.get(gameID);
+            gameObservers.add(connection);
+        } else {
+            ArrayList<Connection> gamePlayer = new ArrayList<>();
+            gamePlayer.add(connection);
+            gameConnections.put(gameID, gamePlayer);
+        }
+    }
+
+    public void remove(Integer gameID, String username) {
+        var gameParticipants = gameConnections.get(gameID);
+        gameParticipants.removeIf(participant -> participant.username.equals(username));
+    }
+
+    public void broadcast(Integer gameID, String excludeUsername, Notification notification) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        var gameParticipants = gameConnections.get(gameID);
+        for (var c : gameParticipants) {
+            if (c.session.isOpen()) {
+                if (!c.username.equals(excludeUsername)) {
+                    c.send(notification.toString());
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            remove(gameID, c.username);
+        }
+    }
 }
